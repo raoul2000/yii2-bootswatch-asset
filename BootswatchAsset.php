415d37e8ec81bddbc8c82fabfcb2a132d7500d02
raoul <manu34@free.fr>
2014-09-28 18:37:16 +0200

    typo

--- BootswatchAsset.php
+++ BootswatchAsset.php
@@ -1,59 +1,63 @@
 <?php
-
 namespace raoul2000\bootswatch;
 
 use Yii;
 use yii\web\AssetBundle;
-use yii\bootstrap\BootstrapAsset;
 use yii\base\InvalidConfigException;
 use yii\base\InvalidCallException;
 
 /**
  * Asset bundle around the Bootswatch theme for bootstrap.
- * 
+ * Use BootswatchAsset::$theme = 'cyborg' to enable the cyborg theme.
+ *
  * @see http://bootswatch.com/
  * @author Raoul
- *
+ *        
  */
 class BootswatchAsset extends AssetBundle
 {
 	const ALIAS = '@vendor/thomaspark/bootswatch';
+
 	/**
+	 *
 	 * @var string name of the active bootswatch theme
 	 */
-	static public $theme = 'amelia';
-	
-    public $depends = [
-        'yii\bootstrap\BootstrapAsset',
-    ];
+	public static $theme = 'amelia';
 
-    /**
-     * Initialize the class properties depending on the current active theme.
-     * @see \yii\web\AssetBundle::init()
-     */
-    public function init()
-    {
-		if ( ! isset(self::$theme)) {
+	public $depends = [
+		'yii\bootstrap\BootstrapAsset'
+	];
+
+	/**
+	 * Initialize the class properties depending on the current active theme.
+	 * 
+	 * @see \yii\web\AssetBundle::init()
+	 */
+	public function init()
+	{
+		if (! isset(self::$theme)) {
 			throw new InvalidConfigException('No theme configured : set BootswatchAsset::$theme to the theme name');
 		}
-    	$this->baseUrl = BootswatchAsset::ALIAS. '/' . self::$theme;
-    	$this->sourcePath = BootswatchAsset::ALIAS .'/' . self::$theme;
-    	$this->css = ['bootstrap.min.css'];
-    	
-    	parent::init();
-    }
+		$this->baseUrl = BootswatchAsset::ALIAS . '/' . self::$theme;
+		$this->sourcePath = BootswatchAsset::ALIAS . '/' . self::$theme;
+		$this->css = [
+			'bootstrap.min.css'
+		];
+		
+		parent::init();
+	}
 
-    /**
-     * Chekcs if a theme is a valid bootswatch theme.
-     * @param string $theme
-     * @return boolean TRUE if $theme is a valid bootswatch theme, FALSE otherwise
-     */
-    static function isSupportedTheme($theme)
-    {
-    	if ( ! is_string($theme) || empty($theme)) {
-    		throw new InvalidCallException("Invalid theme value");
-    	}
-    	return file_exists(Yii::getAlias(BootswatchAsset::ALIAS.'/'.strtolower($theme).'/bootstrap.min.css'));
-    }
-
+	/**
+	 * Chekcs if a theme is a valid bootswatch theme.
+	 * 
+	 * @param string $theme        	
+	 * @return boolean TRUE if $theme is a valid bootswatch theme, FALSE otherwise
+	 */
+	static function isSupportedTheme($theme)
+	{
+		if (! is_string($theme) || empty($theme)) {
+			throw new InvalidCallException('Invalid theme value : empty or null value');
+		}
+		return file_exists(Yii::getAlias(BootswatchAsset::ALIAS . '/' . strtolower($theme) . '/bootstrap.min.css'));
+	}
 }
