ff6d15354f3661e330f8db64368ae88cd51789f1
raoul <raoul_boulard@hotmail.com>
2014-10-06 15:49:38 +0200

    fix gylphicons not accessible.

--- BootswatchAsset.php
+++ BootswatchAsset.php
@@ -8,49 +8,64 @@ use yii\base\InvalidCallException;
 
 /**
  * Asset bundle around the Bootswatch theme for bootstrap.
- * Use BootswatchAsset::$theme = 'cyborg' to enable the cyborg theme.
+ * Use raoul2000\bootswatch\BootswatchAsset::$theme = 'cyborg' to enable the "Cyborg" theme.
  *
  * @see http://bootswatch.com/
  * @author Raoul
- *        
+ *
  */
 class BootswatchAsset extends AssetBundle
 {
-	const ALIAS = '@vendor/thomaspark/bootswatch';
+	const VENDOR_ALIAS = '@vendor/thomaspark/bootswatch';
 
+	public $sourcePath = BootswatchAsset::VENDOR_ALIAS;
 	/**
-	 *
-	 * @var string name of the active bootswatch theme
+	 * @var string name of the active bootswatch theme. When NULL, no bootswatch theme
+	 * is applied.
 	 */
-	public static $theme = 'amelia';
-
+	public static $theme = null;
 	public $depends = [
 		'yii\bootstrap\BootstrapAsset'
 	];
-
 	/**
 	 * Initialize the class properties depending on the current active theme.
-	 * 
+	 *
+	 * When debug mode is disabled, the minified version of the css is used.
 	 * @see \yii\web\AssetBundle::init()
 	 */
 	public function init()
 	{
-		if (! isset(self::$theme)) {
-			throw new InvalidConfigException('No theme configured : set BootswatchAsset::$theme to the theme name');
+		if( self::$theme != null) {
+			if( ! is_string(self::$theme) ) {
+				throw new InvalidConfigException('No theme Bootswatch configured : set BootswatchAsset::$theme to the theme name you want to use');
+			}
+			$this->css = [
+				strtolower(self::$theme).'/bootstrap.'.( YII_DEBUG ? 'min' : '') .'.css'
+			];
+
+			// optimized asset publication : only publish bootswatch theme folders and font folder.
+
+			$this->publishOptions['beforeCopy'] =  function ($from, $to)  {
+				if( is_dir($from)) {
+					$name = pathinfo($from,PATHINFO_BASENAME);
+					return ! in_array($name, ['2','api','assets','bower_components','tests','help','global','default']);
+				}else {
+					$ext = pathinfo($from,PATHINFO_EXTENSION);
+					return in_array($ext,['css','eot','svg','ttf','woff']);
+				}
+			};
 		}
-		$this->baseUrl = BootswatchAsset::ALIAS . '/' . self::$theme;
-		$this->sourcePath = BootswatchAsset::ALIAS . '/' . self::$theme;
-		$this->css = [
-			'bootstrap.min.css'
-		];
-		
 		parent::init();
 	}
 
 	/**
 	 * Chekcs if a theme is a valid bootswatch theme.
-	 * 
-	 * @param string $theme        	
+	 *
+	 * This basic method assumes the $theme name passed as argument is an existing
+	 * subfolder of the vendor alias folder (@vendor/thomaspark/bootswatch) that contains
+	 * the file 'bootstrap.min.css'. If this condition is not true, the theme is invalid.
+	 *
+	 * @param string $theme the theme name to test
 	 * @return boolean TRUE if $theme is a valid bootswatch theme, FALSE otherwise
 	 */
 	static function isSupportedTheme($theme)
@@ -58,6 +73,6 @@ class BootswatchAsset extends AssetBundle
 		if (! is_string($theme) || empty($theme)) {
 			throw new InvalidCallException('Invalid theme value : empty or null value');
 		}
-		return file_exists(Yii::getAlias(BootswatchAsset::ALIAS . '/' . strtolower($theme) . '/bootstrap.min.css'));
+		return file_exists(Yii::getAlias(BootswatchAsset::VENDOR_ALIAS . '/' . strtolower($theme) . '/bootstrap.min.css'));
 	}
 }
--- CHANGE.md
+++ CHANGE.md
@@ -1,2 +1,7 @@
+# version 1.1.0
+- fix : gylphicons not accessible.
+- enh : optimize publication process. When no theme is set, no publication is performed.
+- enh : no theme is selected by default.
+
 # version 1.0.0
 - Initial release
